517460c88761b70befe100cf0634edf2a3599879
tsahi_a <tsahi_a@malam.com>
2022-04-13 16:56:20 +0300

    add history controller

--- Planner/Controllers/GroupController.cs
+++ Planner/Controllers/GroupController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Planner.Service;
 using Planner.Service.API;
 using System.Threading.Tasks;
 
---
+++ Planner/Controllers/HistoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Planner.Controllers
+{
+    [ApiController]
+    [Route("history")]
+    public class HistoryController : BaseController
+    {
+        public HistoryController(ILogger logger, ServiceDomain bl) : base(logger)
+        {
+        }
+    }
+}
