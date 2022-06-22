d1625dc866a0bd11d59f20955c4aa8cbe57b3233
Tsahi Atias <tsahiatias@gmail.com>
2022-06-22 11:33:49 +0300

    fix script

--- Planar/Data/Setup/SqlServerScript.sql
+++ Planar/Data/Setup/SqlServerScript.sql
@@ -693,3 +693,104 @@ USE [master]
 GO
 ALTER DATABASE [Planar] SET  READ_WRITE 
 GO
+---------------------------------------------------
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.JobInstanceLog
+	DROP CONSTRAINT DF_JobInstanceLog_IsStopped
+GO
+CREATE TABLE dbo.Tmp_JobInstanceLog
+	(
+	Id int NOT NULL IDENTITY (1, 1),
+	InstanceId varchar(250) NOT NULL,
+	JobId varchar(20) NOT NULL,
+	JobName varchar(50) NOT NULL,
+	JobGroup varchar(50) NOT NULL,
+	TriggerId varchar(20) NOT NULL,
+	TriggerName varchar(50) NOT NULL,
+	TriggerGroup varchar(50) NOT NULL,
+	ServerName nvarchar(50) NULL,
+	Status int NOT NULL,
+	StatusTitle varchar(10) NULL,
+	StartDate datetime NOT NULL,
+	EndDate datetime NULL,
+	Duration int NULL,
+	EffectedRows int NULL,
+	Data nvarchar(4000) NULL,
+	Information nvarchar(MAX) NULL,
+	Exception nvarchar(MAX) NULL,
+	Retry bit NOT NULL,
+	IsStopped bit NOT NULL
+	)  ON [PRIMARY]
+	 TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE dbo.Tmp_JobInstanceLog SET (LOCK_ESCALATION = TABLE)
+GO
+ALTER TABLE dbo.Tmp_JobInstanceLog ADD CONSTRAINT
+	DF_JobInstanceLog_IsStopped DEFAULT ((0)) FOR IsStopped
+GO
+SET IDENTITY_INSERT dbo.Tmp_JobInstanceLog ON
+GO
+IF EXISTS(SELECT * FROM dbo.JobInstanceLog)
+	 EXEC('INSERT INTO dbo.Tmp_JobInstanceLog (Id, InstanceId, JobId, JobName, JobGroup, TriggerId, TriggerName, TriggerGroup, Status, StatusTitle, StartDate, EndDate, Duration, EffectedRows, Data, Information, Exception, Retry, IsStopped)
+		SELECT Id, InstanceId, JobId, JobName, JobGroup, TriggerId, TriggerName, TriggerGroup, Status, StatusTitle, StartDate, EndDate, Duration, EffectedRows, Data, Information, Exception, Retry, IsStopped FROM dbo.JobInstanceLog WITH (HOLDLOCK TABLOCKX)')
+GO
+SET IDENTITY_INSERT dbo.Tmp_JobInstanceLog OFF
+GO
+DROP TABLE dbo.JobInstanceLog
+GO
+EXECUTE sp_rename N'dbo.Tmp_JobInstanceLog', N'JobInstanceLog', 'OBJECT' 
+GO
+ALTER TABLE dbo.JobInstanceLog ADD CONSTRAINT
+	PK_AutomationTaskCalls PRIMARY KEY CLUSTERED 
+	(
+	Id
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+COMMIT
+---------------------------------------------------
+GO
+CREATE SCHEMA [Admin]
+
+GO
+DROP PROCEDURE [dbo].[FactoryReset]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [Admin].[FactoryReset]
+AS
+delete from  [dbo].[QRTZ_BLOB_TRIGGERS]
+delete from  [dbo].[QRTZ_CALENDARS]
+delete from [dbo].[QRTZ_CRON_TRIGGERS]
+delete from [dbo].[QRTZ_FIRED_TRIGGERS]
+delete from [dbo].[QRTZ_LOCKS]
+delete from [dbo].[QRTZ_PAUSED_TRIGGER_GRPS]
+delete from [dbo].[QRTZ_SCHEDULER_STATE]
+delete from [dbo].[QRTZ_SIMPLE_TRIGGERS]
+delete from [dbo].[QRTZ_SIMPROP_TRIGGERS]
+delete from [dbo].[QRTZ_TRIGGERS]
+delete from [dbo].[QRTZ_JOB_DETAILS]
+
+TRUNCATE TABLE [dbo].[GlobalParameters]
+TRUNCATE TABLE [dbo].[GlobalParameters_Audit]
+TRUNCATE TABLE [dbo].[JobInstanceLog]
+TRUNCATE TABLE [dbo].[MonitorActions]
+TRUNCATE TABLE [dbo].[Trace]
+
+TRUNCATE TABLE [dbo].[UsersToGroups]
+TRUNCATE TABLE [dbo].[Groups]
+TRUNCATE TABLE [dbo].[Users]
