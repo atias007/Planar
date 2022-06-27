4ca0264fbce53d3555b9fdce72a65a08b24e5d7b
Tsahi Atias <tsahiatias@gmail.com>
2022-06-27 16:18:55 +0300

    bug fixes

---
+++ Planar.Service/Data/DataExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Planar.Service.Data
+{
+    public static class DataExtensions
+    {
+        public static void RemoveRange<TEntity>(this DbSet<TEntity> entities, Expression<Func<TEntity, bool>> predicate)
+        where TEntity : class
+        {
+            var records = entities.Where(predicate).ToList();
+            if (records.Count > 0)
+            {
+                entities.RemoveRange(records);
+            }
+        }
+    }
+}
