b1f406494be165be4452087eccfc35f1089eba89
Tsahi Atias <tsahiatias@gmail.com>
2022-07-07 16:44:28 +0300

    add 3

--- Planar.CLI/RestProxy.cs
+++ Planar.CLI/RestProxy.cs
@@ -1,16 +1,27 @@
 ﻿using RestSharp;
 using System.Threading.Tasks;
 using RestSharp.Serializers.NewtonsoftJson;
+using System;
 
 namespace Planar.CLI
 {
     internal class RestProxy
     {
-        private const string baseUrl = "http://localhost:2306";
+        public const string Schema = "http";
+        public static string Host { get; set; } = "localhost";
+        public static int Port { get; set; } = 2306;
+
+        private static Uri BaseUri
+        {
+            get
+            {
+                return new UriBuilder(Schema, Host, Port).Uri;
+            }
+        }
 
         public static async Task<RestResponse<TResponse>> Invoke<TResponse>(RestRequest request)
         {
-            var client = new RestClient(baseUrl);
+            var client = new RestClient(BaseUri);
             client.UseNewtonsoftJson();
             var response = await client.ExecuteAsync<TResponse>(request);
             return response;
@@ -18,7 +29,7 @@ namespace Planar.CLI
 
         public static async Task<RestResponse> Invoke(RestRequest request)
         {
-            var client = new RestClient(baseUrl);
+            var client = new RestClient(BaseUri);
             client.UseNewtonsoftJson();
             var response = await client.ExecuteAsync(request);
             return response;
