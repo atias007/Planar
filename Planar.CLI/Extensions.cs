756ffa1894707a0fb68c3990c9cdd6b16e6c41b8
Tsahi Atias <tsahiatias@gmail.com>
2022-07-07 16:43:23 +0300

    add2

---
+++ Planar.CLI/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planar.CLI
+{
+    internal static class Extensions
+    {
+        public static IEnumerable<string> Split(this string str, Func<char, bool> controller)
+        {
+            int nextPiece = 0;
+
+            for (int c = 0; c < str.Length; c++)
+            {
+                if (controller(str[c]))
+                {
+                    yield return str[nextPiece..c];
+                    nextPiece = c + 1;
+                }
+            }
+
+            yield return str[nextPiece..];
+        }
+
+        public static string TrimMatchingQuotes(this string input, char quote)
+        {
+            if ((input.Length >= 2) &&
+                (input[0] == quote) && (input[^1] == quote))
+                return input[1..^1];
+
+            return input;
+        }
+    }
+}
