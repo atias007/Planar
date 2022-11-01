215014e2cebbe24109140ae47e84aa2de11539be
atias007 <56188592+atias007@users.noreply.github.com>
2022-11-01 15:04:57 +0200

    folder changes (#138)
    * folder changes
    
    * fix yaml
    
    * sonar

---
+++ src/Planar.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Planar.Service.General.Password;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Planar.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestPassword()
+        {
+            var password = PasswordGenerator.GeneratePassword(
+               new PasswordGeneratorBuilder()
+               .IncludeLowercase()
+               .IncludeNumeric()
+               .IncludeSpecial()
+               .IncludeUppercase()
+               .WithLength(12)
+               .Build());
+
+            using (var hmac = new HMACSHA512())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(password);
+                var salt = hmac.Key;
+                var hash = hmac.ComputeHash(passwordBytes);
+            }
+
+            Assert.Pass();
+        }
+    }
+}
