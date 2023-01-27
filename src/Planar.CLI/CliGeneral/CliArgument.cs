6f640db38f3a728698bd6ef97e3c574697bbfefb
jer\tsahi_a <tsahi_a@malam.com>
2023-01-27 14:01:52 +0200

    fix update bug fix help file (service.txt)

--- src/Planar.CLI/CliGeneral/CliArgument.cs
+++ src/Planar.CLI/CliGeneral/CliArgument.cs
@@ -2,9 +2,9 @@
 {
     public class CliArgument
     {
-        public string Key { get; set; }
+        public string Key { get; set; } = string.Empty;
 
-        public string Value { get; set; }
+        public string? Value { get; set; }
 
         public override string ToString()
         {
