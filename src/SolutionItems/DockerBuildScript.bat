5b734d7ca215dfbac8ee751ef742d219372d4f6f
jer\tsahi_a <tsahi_a@malam.com>
2023-03-12 14:22:33 +0200

    fix script

--- src/SolutionItems/DockerBuildScript.bat
+++ src/SolutionItems/DockerBuildScript.bat
@@ -2,6 +2,7 @@
 set version=1.2.0
 
 echo run docker build script for version: %version%
+echo ******** CLEAR OLD ********
 pause
 
 c:
@@ -12,20 +13,27 @@ rmdir /s /q C:\temp\Demo\Planar
 copy C:\Planar\docker\docker-compose.yml C:\temp\Demo\docker-compose.yml /Y
 copy C:\Planar\docker\docker-compose-up.bat C:\temp\Demo\docker-compose-up.bat /Y
 
-cd\Planar\src
+echo ******** BUILD IMAGE ********
 pause
 
+cd\Planar\src
 docker build . -t planar:%version%
+echo ******** TAG IMAGE ********
 pause
 
 docker tag planar:%version% atias007/planar:%version%
 docker tag planar:%version% atias007/planar:latest
+
+echo ******** PUSH IMAGE ********
 pause
 
 docker login
-docker push atias007/%version%
-docker push atias007/latest
+docker push atias007/planar:%version%
+docker push atias007/planar:latest
+echo ******** RUN DOCKER COMPOSE ********
 pause
 
+cd\temp\Demo
+docker-compose-up.bat
 echo *** DONE ***
 pause
