8bf9bf4923209443efa14378bf225323fd896195
jer\tsahi_a <tsahi_a@malam.com>
2024-05-02 15:38:35 +0300

    s

--- nuget packages/Publish.bat
+++ nuget packages/Publish.bat
@@ -1,14 +1,57 @@
-@echo off
+@echo
+
+set /p apikey="Enter API key:"
+set /p version="Enter version number:"
+
+@echo apikey=%apikey%
+@echo version=%version%
+
+@echo -- > Press any key to continue...
+pause
 
 c:
-cd\Planar\nuget packages\publish
-del *.*
+Cd\Planar\nuget packages
 
-dotnet publish "C:\Planar\nuget packages\Planar.Common\Planar.Common.csproj" --configuration Release --output "c:\Planar\nuget packages\publish"
-dotnet publish "C:\Planar\nuget packages\Planar.Job\Planar.Job.csproj" --configuration Release --output "c:\Planar\nuget packages\publish"
-dotnet publish "C:\Planar\nuget packages\Planar.Job.Test\Planar.Job.Test.csproj" --configuration Release --output "c:\Planar\nuget packages\publish"
-dotnet publish "C:\Planar\nuget packages\Planar.Monitor.Hook\Planar.Monitor.Hook.csproj" --configuration Release --output "c:\Planar\nuget packages\publish"
+rmdir "publish" /S /Q
 
+cd Planar.Client
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
 
+cd Planar.Common
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
 
+cd Planar.Hook
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
+
+cd Planar.Hook.Test
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
+
+cd Planar.Job
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
+
+cd Planar.Job.Test
+dotnet publish --output "C:\Planar\nuget packages\publish"
+dotnet pack --output "C:\Planar\nuget packages\publish"
+cd..
+
+cd publish
+
+@echo -- > Press any key to continue...
 pause
+
+dotnet nuget push Planar.Client.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
+dotnet nuget push Planar.Common.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
+dotnet nuget push Planar.Hook.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
+dotnet nuget push Planar.Job.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
+dotnet nuget push Planar.Hook.Test.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
+dotnet nuget push Planar.Job.Test.%version%.nupkg --api-key %apikey% --source https://api.nuget.org/v3/index.json
