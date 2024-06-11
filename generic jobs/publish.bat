4eae0cef7fe98d80a2fa5a3234ca3508592bcafa
jer\tsahi_a <tsahi_a@malam.com>
2024-06-11 09:32:06 +0300

    version

--- generic jobs/publish.bat
+++ generic jobs/publish.bat
@@ -1,21 +1,33 @@
 c:
 cd\Planar\generic jobs
 cd FolderCheck
-dotnet publish -c Release -o C:\temp\Planar\publish\FolderCheck
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Monitoring\FolderCheck
 cd..
 
 cd HealthCheck
-dotnet publish -c Release -o C:\temp\Planar\publish\HealthCheck
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Monitoring\HealthCheck
 cd..
 
 cd RabbitMQCheck
-dotnet publish -c Release -o C:\temp\Planar\publish\RabbitMQCheck
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Monitoring\RabbitMQCheck
 cd..
 
 cd RedisCheck
-dotnet publish -c Release -o C:\temp\Planar\publish\RedisCheck
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Monitoring\RedisCheck
 cd..
 
 cd SqlQueryCheck
-dotnet publish -c Release -o C:\temp\Planar\publish\SqlQueryCheck
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Monitoring\SqlQueryCheck
+cd..
+
+cd FolderRetention
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Operation\FolderRetention
+cd..
+
+cd RedisOperations
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Operation\RedisOperations
+cd..
+
+cd SqlTableRetention
+dotnet publish -c Release -o C:\temp\Planar\publish\GenericJobs\Operation\SqlTableRetention
 cd..
