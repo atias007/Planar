2d12980f7644efb78d1c2be6fba8756ac56f8fbd
jer\tsahi_a <tsahi_a@malam.com>
2024-09-05 16:33:45 +0300

    add cir-break to client nuget

---
+++ nuget packages/Planar.Client/Entities/JobCircuitBreaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Planar.Client.Entities
+{
+    public class JobCircuitBreaker
+    {
+        public int FailureThreshold { get; set; }
+        public int SuccessThreshold { get; set; }
+        public TimeSpan? PauseSpan { get; set; }
+        public int FailCounter { get; set; }
+        public int SuccessCounter { get; set; }
+        public DateTime? WillBeResetAt { get; set; }
+        public DateTime? ActivatedAt { get; set; }
+        public bool Activated => ActivatedAt.HasValue;
+    }
+}
