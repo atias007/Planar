faab25e9eee70790cc2e183d65f79c456025de63
jer\tsahi_a <tsahi_a@malam.com>
2024-10-23 13:06:05 +0300

    add sqlite

---
+++ src/DbUp/dbup-sqlite/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f949414-f078-49bf-b50e-a3859c18fb6e")]
---
+++ src/DbUp/dbup-sqlite/SQLiteObjectParser.cs
@@ -0,0 +1,15 @@
+﻿using DbUp.Support;
+
+namespace DbUp.SQLite
+{
+    /// <summary>
+    /// Parses Sql Objects and performs quoting functions.
+    /// </summary>
+    public class SQLiteObjectParser : SqlObjectParser
+    {
+        public SQLiteObjectParser()
+            : base("[", "]")
+        {
+        }
+    }
+}
