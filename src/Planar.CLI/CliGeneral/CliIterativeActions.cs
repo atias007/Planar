9fbb74cea9d28472f3518b425295938b60183e4e
jer\tsahi_a <tsahi_a@malam.com>
2024-11-07 23:48:59 +0200

    e

--- src/Planar.CLI/CliGeneral/CliIterativeActions.cs
+++ src/Planar.CLI/CliGeneral/CliIterativeActions.cs
@@ -1,86 +1,143 @@
 ﻿using Planar.CLI.Actions;
+using Planar.CLI.CliGeneral;
 using Planar.CLI.Entities;
 using Spectre.Console;
-using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Planar.CLI
+namespace Planar.CLI;
+
+public static class CliIterativeActions
 {
-    public static class CliIterativeActions
+    public static async Task InvokeGetRunnings(CliGetRunningJobsRequest param, CancellationToken cancellationToken)
     {
-        public static async Task InvokeGetRunnings(CliGetRunningJobsRequest param, CancellationToken cancellationToken)
-        {
-            var result = await JobCliActions.GetRunningJobsInner(param, cancellationToken);
-            var data = result.Item1;
+        var result = await JobCliActions.GetRunningJobsInner(param, cancellationToken);
+        var data = result.Item1;
 
-            var ids = data.Select(r => r.Id).ToList();
-            var table = CliTableExtensions.GetTable(result.Item1);
-            await AnsiConsole.Live(table.Table).StartAsync(async context =>
+        var table = CliTableExtensions.GetTable(data);
+        table.DataSource = data.Select(i => i.FireInstanceId).ToList();
+        await AnsiConsole.Live(table.Table).StartAsync(async context =>
+        {
+            context.Refresh();
+            if (data.Count == 0) { return; }
+            await Task.Delay(2000, cancellationToken);
+
+            var counter = 0;
+            while (counter < 1000)
             {
+                var isAllFinish = await RefreshTable(param, table, cancellationToken);
                 context.Refresh();
-                if (ids.Count == 0) { return; }
-                await Task.Delay(2000, cancellationToken);
 
-                var counter = 0;
-                while (counter < 1000)
+                if (isAllFinish) { break; }
+                var delay = counter switch
                 {
-                    var isAllFinish = await RefreshTable(param, table.Table, ids, cancellationToken);
-                    context.Refresh();
+                    >= 0 and <= 60 => 1000,
+                    > 60 and <= 180 => 2000,
+                    > 180 and <= 500 => 4_000,
+                    > 500 and <= 1000 => 10_000,
+                    _ => 30_000
+                };
 
-                    if (isAllFinish) { break; }
-                    var delay = counter switch
-                    {
-                        >= 0 and <= 60 => 1000,
-                        > 60 and <= 180 => 2000,
-                        > 180 and <= 500 => 4_000,
-                        > 500 and <= 1000 => 10_000,
-                        _ => 30_000
-                    };
-
-                    await Task.Delay(delay, cancellationToken);
-                    counter++;
-                }
-            });
-        }
-
-        private static async Task<bool> RefreshTable(CliGetRunningJobsRequest param, Table table, List<string> ids, CancellationToken cancellationToken)
-        {
-            var refreshResult = await JobCliActions.GetRunningJobsInner(param, cancellationToken);
-            var refreshData = refreshResult.Item1;
-
-            for (int i = 0; i < table.Rows.Count; i++)
-            {
-                RefreshTableRow(table, ids, refreshData, i);
+                await Task.Delay(delay, cancellationToken);
+                counter++;
             }
 
-            var isAllFinish = refreshData?.TrueForAll(r => r.Progress == 100);
-            return isAllFinish.GetValueOrDefault();
+            SetAllCompleted(table.Table);
+            context.Refresh();
+        });
+    }
+
+    private static async Task<bool> RefreshTable(CliGetRunningJobsRequest param, CliTable table, CancellationToken cancellationToken)
+    {
+        var refreshResult = await JobCliActions.GetRunningJobsInner(param, cancellationToken);
+        var newData = refreshResult.Item1;
+        var existsData = table.DataSource as List<string>;
+        if (newData == null || existsData == null) { return true; }
+
+        // set completed rows
+        for (int i = 0; i < existsData.Count; i++)
+        {
+            var fireInstanceId = existsData[i];
+            var index = newData.FindIndex(r => r.FireInstanceId == fireInstanceId);
+            if (index == -1)
+            {
+                UpdateCompletedTableRow(table.Table, i);
+            }
         }
 
-        private static void RefreshTableRow(Table table, List<string> ids, List<API.Common.Entities.RunningJobDetails>? refreshData, int i)
+        // add & update existsing rows
+        foreach (var item in newData)
         {
-            if (refreshData == null) { return; }
-            var id = ids[i];
-            var item = refreshData.Find(r => r.Id == id.ToString());
-            if (item == null || item.Progress == 100)
+            var index = existsData.FindIndex(r => r == item.FireInstanceId);
+            if (index == -1)
             {
-                table.UpdateCell(i, 3, $"[grey]completed[/]");
-                table.UpdateCell(i, 4, "---");
-                table.UpdateCell(i, 5, "---");
-                table.UpdateCell(i, 6, "---");
-                table.UpdateCell(i, 7, "[grey]---[/]");
+                AddTableRow(table.Table, item);
+                existsData.Add(item.FireInstanceId);
             }
             else
             {
-                table.UpdateCell(i, 3, CliTableFormat.GetProgressMarkup(item.Progress));
-                table.UpdateCell(i, 4, CliTableFormat.FormatNumber(item.EffectedRows));
-                table.UpdateCell(i, 5, CliTableFormat.FormatExceptionCount(item.ExceptionsCount));
-                table.UpdateCell(i, 6, CliTableFormat.FormatTimeSpan(item.RunTime));
-                table.UpdateCell(i, 7, $"[grey]CliTableFormat.FormatTimeSpan(item.EstimatedEndTime)[/]");
+                RefreshTableRow(table.Table, index, item);
             }
         }
+
+        table.DataSource = existsData;
+        var isAllFinish = newData.TrueForAll(r => r.Progress == 100);
+        return isAllFinish;
+    }
+
+    private static void RefreshTableRow(Table table, int index, API.Common.Entities.RunningJobDetails data)
+    {
+        if (data == null || data.Progress == 100)
+        {
+            UpdateCompletedTableRow(table, index);
+            return;
+        }
+
+        table.UpdateCell(index, 3, CliTableFormat.GetProgressMarkup(data.Progress));
+        table.UpdateCell(index, 4, CliTableFormat.FormatNumber(data.EffectedRows));
+        table.UpdateCell(index, 5, CliTableFormat.FormatExceptionCount(data.ExceptionsCount));
+        table.UpdateCell(index, 6, CliTableFormat.FormatTimeSpan(data.RunTime));
+        table.UpdateCell(index, 7, $"[grey]{CliTableFormat.FormatTimeSpan(data.EstimatedEndTime)}[/]");
+    }
+
+    private static void SetAllCompleted(Table table)
+    {
+        var rowsCount = table.Rows.Count;
+        for (int i = 0; i < rowsCount; i++)
+        {
+            UpdateCompletedTableRow(table, i);
+        }
+    }
+
+    private static void UpdateCompletedTableRow(Table table, int index)
+    {
+        if (index < 0)
+        {
+            Debugger.Break();
+            return;
+        }
+
+        table.UpdateCell(index, 3, $"[grey]completed[/]");
+        table.UpdateCell(index, 4, "---");
+        table.UpdateCell(index, 5, "---");
+        table.UpdateCell(index, 6, "---");
+        table.UpdateCell(index, 7, "[grey]---[/]");
+    }
+
+    private static void AddTableRow(Table table, API.Common.Entities.RunningJobDetails data)
+    {
+        if (data == null) { return; }
+        table.AddRow(
+            CliTableFormat.GetFireInstanceIdMarkup(data.FireInstanceId),
+            $"{data.Id}",
+            CliTableFormat.FormatJobKey(data.Group, data.Name),
+            CliTableFormat.GetProgressMarkup(data.Progress),
+            CliTableFormat.FormatNumber(data.EffectedRows),
+            CliTableFormat.FormatExceptionCount(data.ExceptionsCount),
+            CliTableFormat.FormatTimeSpan(data.RunTime),
+            $"[grey]{CliTableFormat.FormatTimeSpan(data.EstimatedEndTime)}[/]");
     }
 }
--- src/Planar.CLI/CliGeneral/CliTable.cs
+++ src/Planar.CLI/CliGeneral/CliTable.cs
@@ -1,49 +1,50 @@
 ﻿using Planar.API.Common.Entities;
 using Spectre.Console;
 
-namespace Planar.CLI.CliGeneral
+namespace Planar.CLI.CliGeneral;
+
+public class CliTable
 {
-    public class CliTable
+    public CliTable()
     {
-        public CliTable()
-        {
-            Table = new Table();
-        }
-
-        public CliTable(Table table)
-        {
-            Table = table;
-        }
-
-        public CliTable(bool showCount) : this()
-        {
-            ShowCount = showCount;
-        }
-
-        public CliTable(bool showCount, string entityName) : this(showCount)
-        {
-            EntityName = entityName;
-        }
-
-        public CliTable(PagingResponse? paging, string entityName) : this(showCount: true)
-        {
-            EntityName = entityName;
-            Paging = paging;
-        }
-
-        public CliTable(PagingResponse? paging) : this(showCount: true)
-        {
-            Paging = paging;
-        }
-
-        public Table Table { get; }
-
-        public bool ShowCount { get; }
-
-        public string? EntityName { get; }
-
-        public string? Title { get; set; }
-
-        public PagingResponse? Paging { get; }
+        Table = new Table();
     }
+
+    public CliTable(Table table)
+    {
+        Table = table;
+    }
+
+    public CliTable(bool showCount) : this()
+    {
+        ShowCount = showCount;
+    }
+
+    public CliTable(bool showCount, string entityName) : this(showCount)
+    {
+        EntityName = entityName;
+    }
+
+    public CliTable(PagingResponse? paging, string entityName) : this(showCount: true)
+    {
+        EntityName = entityName;
+        Paging = paging;
+    }
+
+    public CliTable(PagingResponse? paging) : this(showCount: true)
+    {
+        Paging = paging;
+    }
+
+    public Table Table { get; }
+
+    public bool ShowCount { get; }
+
+    public string? EntityName { get; }
+
+    public string? Title { get; set; }
+
+    public PagingResponse? Paging { get; }
+
+    public object? DataSource { get; set; }
 }
