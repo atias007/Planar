2125c1acc7dea9c1a21c6720738fbe36fbd7c50e
jer\tsahi_a <tsahi_a@malam.com>
2025-05-09 11:10:25 +0300

    er

--- src/Jobs/CommonJob/ExceptionDto.cs
+++ src/Jobs/CommonJob/ExceptionDto.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 
-namespace Planar;
-
-public class ExceptionDto
+// *** DONT CHANGE TO FILENAME SCOPE NAMESPACE ===
+namespace Planar
 {
-    public ExceptionDto()
+    public class ExceptionDto
     {
-    }
+        public ExceptionDto()
+        {
+        }
 
-    public ExceptionDto(Exception ex)
-    {
-        Message = ex.Message;
-        ExceptionText = ex.ToString();
-    }
+        public ExceptionDto(Exception ex)
+        {
+            Message = ex.Message;
+            ExceptionText = ex.ToString();
+        }
 
 #if NETSTANDARD2_0
-    public string Message { get; set; } = string.Empty;
-    public string ExceptionText { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string ExceptionText { get; set; } = string.Empty;
 #else
     public string? Message { get; set; } = string.Empty;
     public string? ExceptionText { get; set; } = string.Empty;
 
     public long Length => (Message?.Length ?? 0) + (ExceptionText?.Length ?? 0);
 #endif
+    }
 }
