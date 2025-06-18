c49708c2904960f96aed1b4030b5c2e6aa1d7e37
jer\tsahi_a <tsahi_a@malam.com>
2025-06-18 16:59:57 +0300

    add project

---
+++ src/Planar.Watcher/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Planar.Watcher;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("logs/planar.watcher.log", rollingInterval: RollingInterval.Month)
+            .CreateLogger();
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Planar.Watcher";
+});
+
+builder.Services.AddHostedService<WatcherService>();
+builder.Services.AddLogging(builder =>
+{
+    builder.ClearProviders();
+    builder.AddSerilog();
+});
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var host = builder.Build();
+await host.RunAsync();
---
+++ src/Planar.Watcher/WatcherService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Planar.Watcher;
+
+internal class WatcherService(ILogger<WatcherService> logger) : BackgroundService
+{
+    private readonly TimeSpan _interval = TimeSpan.FromMinutes(1);
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Console.Out.WriteLineAsync("----------------------------------");
+        await Console.Out.WriteLineAsync("- Planar watcher service started -");
+        await Console.Out.WriteLineAsync("----------------------------------");
+        logger.LogInformation("Planar watcher service started with interval {Interval}", _interval);
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(_interval, stoppingToken);
+            Console.WriteLine("x");
+        }
+    }
+}
