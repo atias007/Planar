2b96f14649275dcee4b11300ebbda8c46e31afd6
jer\tsahi_a <tsahi_a@malam.com>
2025-07-25 09:35:08 +0300

    gen jobs

--- generic jobs/Common/WindowsServiceUtil.cs
+++ generic jobs/Common/WindowsServiceUtil.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using System.ComponentModel;
 using System.Globalization;
 using System.Management;
 using System.Runtime.InteropServices;
@@ -6,45 +9,35 @@ using System.ServiceProcess;
 
 namespace Common;
 
+#pragma warning disable CA1416 // Validate platform compatibility
+
 public sealed class WindowsServiceUtil(ILogger logger, string serviceName, string host)
 {
-    public ServiceControllerStatus WaitForStatus(ServiceController controller, ServiceControllerStatus status, TimeSpan timeout, bool restart)
-    {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            throw new PlatformNotSupportedException("This method is only supported on Windows.");
-        }
+    private static readonly RetryPolicy _controllerRetry = Policy
+        .Handle<Win32Exception>(ex => ex.Message.Contains("The service cannot accept control messages at this time"))
+        .WaitAndRetry(3, count => TimeSpan.FromSeconds(count));
 
-        try
-        {
-            controller.WaitForStatus(status, timeout);
-            controller.Refresh();
-            return controller.Status;
-        }
-        catch (System.ServiceProcess.TimeoutException)
-        {
-            if (!restart) { throw new CheckException($"timeout waiting for status {status} of service {controller.ServiceName}"); }
-#pragma warning disable S6667 // Logging in a catch clause should pass the caught exception as a parameter.
-            logger.LogWarning("service '{Name}' on host '{Host}' is in {Status} status for long time. kill the process", serviceName, host, controller.Status);
-#pragma warning restore S6667 // Logging in a catch clause should pass the caught exception as a parameter.
-            KillServiceProcess();
-            return WaitForStatus(controller, ServiceControllerStatus.Stopped, timeout, restart: false);
-        }
+    public static void Continue(ServiceController controller)
+    {
+        ValidateWindowsServiceOs();
+
+        PerformeControllerAction(controller, c => c.Continue());
     }
 
-    public void KillServiceProcess()
+    public static void KillServiceProcess(string host, string serviceName)
     {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            throw new PlatformNotSupportedException("This method is only supported on Windows.");
-        }
+        ValidateWindowsServiceOs();
 
         var options = new ConnectionOptions { EnablePrivileges = true };
+
         var scope = new ManagementScope($"\\\\{host}\\root\\cimv2", options);
+
         scope.Connect();
 
         var query = new ObjectQuery($"SELECT * FROM Win32_Service WHERE Name='{serviceName}'");
+
         using var searcher = new ManagementObjectSearcher(scope, query);
+
         using var results = searcher.Get();
 
         foreach (var item in results)
@@ -57,22 +50,57 @@ public sealed class WindowsServiceUtil(ILogger logger, string serviceName, strin
             try
             {
                 var processObjGetOpt = new ObjectGetOptions();
+
                 var processPath = new ManagementPath("Win32_Process");
+
                 using var processClass = new ManagementClass(scope, processPath, processObjGetOpt);
+
                 using var processInParams = processClass.GetMethodParameters("Create");
+
                 processInParams["CommandLine"] = $"cmd /c \"taskkill /f /pid {processId}\"";
+
                 using var outParams = processClass.InvokeMethod("Create", processInParams, null);
+
                 int returnCode = Convert.ToInt32(outParams["returnValue"], CultureInfo.CurrentCulture);
+
                 if (returnCode != 0)
                 {
-                    logger.LogWarning("Error killing process: {ReturnCode}", returnCode);
+                    Console.WriteLine("Error killing process: " + returnCode);
                 }
             }
             catch (Exception ex)
             {
-                logger.LogCritical(ex, "Error killing process");
+                Console.WriteLine(ex);
             }
         }
+    }
+
+    public static void Refresh(ServiceController controller)
+    {
+        ValidateWindowsServiceOs();
+
+        PerformeControllerAction(controller, c => c.Refresh());
+    }
+
+    public static void Start(ServiceController controller)
+    {
+        ValidateWindowsServiceOs();
+
+        PerformeControllerAction(controller, c => c.Start());
+    }
+
+    public static void Stop(ServiceController controller)
+    {
+        ValidateWindowsServiceOs();
+
+        PerformeControllerAction(controller, c => c.Stop());
+    }
+
+    public static void Stop(ServiceController controller, bool stopDependentServices)
+    {
+        ValidateWindowsServiceOs();
+
+        PerformeControllerAction(controller, c => c.Stop(stopDependentServices));
     }
 
     public static void ValidateWindowsServiceOs()
@@ -81,5 +109,66 @@ public sealed class WindowsServiceUtil(ILogger logger, string serviceName, strin
         {
             throw new PlatformNotSupportedException("this application is only supported on Windows.");
         }
+    }
+
+    public ServiceControllerStatus WaitForStatus(ServiceController controller, ServiceControllerStatus status, TimeSpan timeout, bool restart)
+    {
+        ValidateWindowsServiceOs();
+
+        try
+        {
+            PerformeControllerAction(controller, c => c.WaitForStatus(status, timeout));
+
+            PerformeControllerAction(controller, c => c.Refresh());
+
+            return controller.Status;
+        }
+        catch (System.ServiceProcess.TimeoutException)
+        {
+            if (!restart) { throw new CheckException($"timeout waiting for status {status} of service {controller.ServiceName}"); }
+
+#pragma warning disable S6667 // Logging in a catch clause should pass the caught exception as a parameter.
+
+            logger.LogWarning("service '{Name}' on host '{Host}' is in {Status} status for long time. kill the process", serviceName, host, controller.Status);
+
+#pragma warning restore S6667 // Logging in a catch clause should pass the caught exception as a parameter.
+
+            KillServiceProcess(host, serviceName);
+
+            return RecoverServiceStatusAfterKill(controller).Result;
+        }
+    }
+
+    private static void PerformeControllerAction(ServiceController controller, Action<ServiceController> action)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            throw new PlatformNotSupportedException("This method is only supported on Windows.");
+        }
+
+        _controllerRetry.Execute(() => action.Invoke(controller));
+    }
+
+    private static async Task<ServiceControllerStatus> RecoverServiceStatusAfterKill(ServiceController controller)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            throw new PlatformNotSupportedException("This method is only supported on Windows.");
+        }
+
+        await Task.Delay(10_000);
+
+        if (controller.Status == ServiceControllerStatus.Running) { return ServiceControllerStatus.Running; }
+
+        if (controller.Status == ServiceControllerStatus.Stopped)
+        {
+            Start(controller);
+
+            PerformeControllerAction(controller, c => c.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(3)));
+
+            PerformeControllerAction(controller, c => c.Refresh());
+        }
+
+        return controller.Status;
     }
 }
